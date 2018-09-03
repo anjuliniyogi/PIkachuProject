db7638bdb957025ca43d7eb9e7e9463a71d538b4
anjuli <anjuli@anjulis-air>
2018-09-03 11:31:11 -0700

    commiting pikachuClass.java

---
+++ src/PikachuClass.java
@@ -0,0 +1,14 @@
+// Anjuli Niyogi CompSci 2nd period
+// 08/31/2018
+public class PikachuClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("\t" + "Pikachu welcomes you to the world of Pokemon! ");
+		System.out.println("\t" + "\t" + "\t" + "(\\__/)");
+		System.out.println("\t" + "\t" + "\t" + "(o^.^)");
+		System.out.println("\t" + "\t" + "\t" + "z(_(\")(\")");
+	}
+
+}
